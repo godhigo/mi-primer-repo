c383f70fae3d84fa707535d5d7b5706e83941f5b
Diego Navarro <godhieguito@gmail.com>
2024-10-16 15:32:10 -0600

    Si

--- assets/Codigos/ahorcado.cpp
+++ assets/Codigos/ahorcado.cpp
@@ -4,12 +4,12 @@ using namespace std;
 const int MAX_INTENTOS = 6; //Número máximo de intentos
 
 //Función para mostrar el estado actual del juego
-void mostrarEstado(const char* palabraSecreta, const bool* letrasAdivinadas, int longitud) {
-    for (int i = 0; i < longitud; ++i) {
-        if (letrasAdivinadas[i]) {
-            cout << palabraSecreta[i] << " "; //Mostrar letra si ha sido adivinada
-        } else {
-            cout << "_ "; //Mostrar guion bajo si no se ha adivinado
+void mostrarEstado(const char* palabraSecreta, const bool* letrasAdivinadas, int longitud) { //Apuntador hacia el valor de palabra secreta, un apuntador booleano para ir letra por letra de la palabra secreta y el int es para la longitud de la palabra
+    for (int i = 0; i < longitud; ++i) { //Se recorre letra por letra
+        if (letrasAdivinadas[i]) { //Si se encuentra la misma letra que el usuario ingreso entonces
+            cout << palabraSecreta[i] << " "; //Se muestra la letra adivinada
+        } else { //Si la letra que el usuario ingreso no está en la palabra 
+            cout << "_ "; //Se muestra un guion bajo
         }
     }
     cout << endl;
@@ -21,17 +21,17 @@ int main() {
     bool letrasAdivinadas[longitudPalabra] = {false}; //Inicializar letras adivinadas
     int intentosRestantes = MAX_INTENTOS; //Intentos permitidos
 
-    cout << "¡Bienvenido al juego de Ahorcados!" << endl;
+    cout << "¡Bienvenido al juego de Ahorcados!" << endl; //Bienvenida al usuario
 
     //Bucle principal del juego
-    while (intentosRestantes > 0) {
-        cout << "\nPalabra: ";
-        mostrarEstado(palabraSecreta, letrasAdivinadas, longitudPalabra); 
+    while (intentosRestantes > 0) { //Mientras que los intentos restantes sean mayores a 0 se seguirá ejecutando
+        cout << "\nPalabra: "; 
+        mostrarEstado(palabraSecreta, letrasAdivinadas, longitudPalabra); //Llamamos a la funcion
         cout << "Intentos restantes: " << intentosRestantes << endl;
 
-        char letra;
-        cout << "Adivina una letra: ";
-        cin >> letra;
+        char letra; //Aquí declaramos la variable letra
+        cout << "Adivina una letra: "; 
+        cin >> letra; //Aquí guardamos el valor ingresado por el usuario (letra)
 
         bool letraEncontrada = false;
 
@@ -43,32 +43,4 @@ int main() {
             }
         }
 
-        //Verificar si la letra fue encontrada
-        if (!letraEncontrada) {
-            intentosRestantes--; //Restar un intento si la letra no está
-            cout << "¡Incorrecto! La letra no está en la palabra." << endl;
-        } else {
-            cout << "¡Correcto! La letra está en la palabra." << endl;
-        }
-
-        //Verificar si el jugador ha adivinado la palabra
-        bool adivinada = true;
-        for (int i = 0; i < longitudPalabra; ++i) {
-            if (!letrasAdivinadas[i]) {
-                adivinada = false; //Si hay una letra que no ha sido adivinada
-                break;
-            }
-        }
-
-        if (adivinada) {
-            cout << "¡Felicidades! Has adivinado la palabra: " << palabraSecreta << endl;
-            break;
-        }
-    }
-
-    if (intentosRestantes == 0) {
-        cout << "¡Game over! La palabra era: " << palabraSecreta << endl;
-    }
-
-    return 0;
-}
+       
